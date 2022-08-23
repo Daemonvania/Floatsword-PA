a17506c1f7e00caeac84703b2a4b0e3afa8d0bec
Cowboy <peteran20@outlook.com>
2022-08-24 01:06:33 +0300

    Added Basic Enemy stuff

--- Assets/NormalEnemyAI.cs
+++ Assets/NormalEnemyAI.cs
@@ -4,7 +4,18 @@ using UnityEngine;
 
 public class NormalEnemyAI : MonoBehaviour
 {
-    public GameObject torso;
+    public Sprite bodyMiddle;
+    public Sprite handMiddle;
+    
+    public Sprite bodyRight;
+    public Sprite handRight;
+    
+    public Sprite bodyLeft;
+    public Sprite handLeft;
+    
+    [Space]
+    public GameObject hand;
+    public GameObject body;
 
     private GameObject player;
 
@@ -18,6 +29,17 @@ public class NormalEnemyAI : MonoBehaviour
     void Update()
     { 
         Vector2 direction = new Vector2(player.transform.position.x - transform.position.x, player.transform.position.z - transform.position.y);
-       torso.transform.right = direction;
+       hand.transform.right = direction;
+
+       if (direction.x < 0)
+       {
+           hand.GetComponentInChildren<SpriteRenderer>().sprite = handLeft;
+           body.GetComponent<SpriteRenderer>().sprite = bodyLeft;
+       }
+       if (direction.x > 0)
+       {
+           hand.GetComponentInChildren<SpriteRenderer>().sprite = handRight;
+           body.GetComponent<SpriteRenderer>().sprite = bodyRight;
+       }
     }
 }
